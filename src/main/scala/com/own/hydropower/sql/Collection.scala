5218aa9fbabc014c31196e2b969d8c48222805d1
tuigerphkeeper <lyj1427405059@163.com>
2020-06-12 00:08:45 +0800

    修改

--- src/main/scala/com/own/hydropower/sql/Collection.scala
+++ src/main/scala/com/own/hydropower/sql/Collection.scala
@@ -11,44 +11,44 @@ object Collection {
 
   private val log = LogManager.getLogger("Collocation")
 
-  var hive_dir: String = null //hive????��??
-  var toHdfsRoot: String = null //??????hdfs??sql
-  var toHdfsTable: String = null
-  var businessType: String = null
-  var sql_toHiveTable: String = null
-  var sql_toHiveFromTable: String = null
+  var hive_dir: String = _
+  var hdfsPath: String = _
+  var hdfsTable: String = _
+  var businessType: String = _
+  var sql_toHiveTable: String = _
+  var sql_toHiveFromTable: String = _
 
   /**
-   * ????????��???????
+   * 初始化信息
    */
   def initConfig() {
-    log.info("????????????...")
+    log.info("初始化配置信息")
     BasicConfigurator.configure()
     readCommons()
   }
 
   /**
-   * ???sparkSQL.properties??????????
+   * 读取spark_sql.properties，加载到内存
    */
   private def readCommons() {
-    val pp = new Properties
+    val pp = new Properties()
     try {
-      log.info("???log4j.properties...")
-      PropertyConfigurator.configure("/usr/kbd/toHdfs/log4j.properties")
-      log.info("???sparkSQL.properties???????...")
-      pp.load(new FileInputStream("/usr/kbd/toHdfs/sparkSQL.properties"))
+      //      PropertyConfigurator.configure("/usr/kbd/toHdfs/log4j.properties")
+      //      pp.load(new FileInputStream("/usr/kbd/toHdfs/sparkSQL.properties"))
+      PropertyConfigurator.configure("src\\main\\resources\\log4j.properties")
+      pp.load(new FileInputStream("src\\main\\resources\\spark_sql.properties"))
     } catch {
       case e: IOException =>
         log.error(e)
     }
     if (pp != null) {
       hive_dir = pp.getProperty("hive_dir")
-      toHdfsRoot = pp.getProperty("tohdfsroot")
-      toHdfsTable = pp.getProperty("tohdfstable")
+      hdfsPath = pp.getProperty("hdfsPath")
+      hdfsTable = pp.getProperty("hdfsTable")
       businessType = pp.getProperty("BusinessType")
       sql_toHiveTable = pp.getProperty("sql_toHiveTable")
       sql_toHiveFromTable = pp.getProperty("sql_toHiveFromTable")
     }
-    else log.error("???sparkSQL.xml??????????????")
+    else log.error("spark_sql.properties配置文件读取错误")
   }
 }
--- src/main/scala/com/own/hydropower/sql/Start_sql.scala
+++ src/main/scala/com/own/hydropower/sql/Start_sql.scala
@@ -8,20 +8,21 @@ import org.apache.spark.sql.SparkSession
 
 
 object Start_sql {
-  private val log = LogManager.getLogger("Main")
-  //获取配置信息
+
+  private val log = LogManager.getLogger(Start_sql.getClass)
+  Collection.initConfig()
 
   def main(args: Array[String]): Unit = {
     Collection.initConfig()
     log.info("加载sparkSQL配置...")
     val sparkSession: SparkSession = SparkSession.builder()
-      //     .master("local[2]")
+      .master("local[2]")
       .appName("sparkSQL")
       .config("spark.sql.warehouse.dir", Collection.hive_dir)
       .enableHiveSupport()
       .getOrCreate()
     dataToHdfs(sparkSession)
-    dataToHive(sparkSession)
+    // dataToHive(sparkSession)
   }
 
   def dataToHdfs(sparkSession: SparkSession): Unit = {
@@ -30,11 +31,11 @@ object Start_sql {
     val dateFormat: SimpleDateFormat = new SimpleDateFormat("yyyyMMdd")
     val date = dateFormat.format(now)
     log.info("开始数据导出到hdfs...")
-    val rootpath = Collection.toHdfsRoot
-    val sql = "insert overwrite directory " + "'" + rootpath + Collection.businessType + date + "/" + "'" + " ROW FORMAT DELIMITED FIELDS TERMINATED BY '\\t' select * from " + Collection.toHdfsTable
+    val sql = "insert overwrite directory " + "'" + Collection.hdfsPath + Collection.businessType + date + "/" + "'" +
+      " ROW FORMAT DELIMITED FIELDS TERMINATED BY '\\t' select * from " + "default." + Collection.hdfsTable
     println("tohdfs:  " + sql)
     sparkSession.sql(sql)
-    val cmd = "hadoop fs -rm -f " + rootpath + Collection.businessType + date + "/_SUCCESS"
+    val cmd = "hadoop fs -rm -f " + Collection.hdfsPath + Collection.businessType + date + "/_SUCCESS"
     println("cmd: " + cmd)
     Runtime.getRuntime.exec(cmd)
     log.info("导出结束...，共耗时:" + (System.currentTimeMillis() - time1) / 1000 + "秒。")
